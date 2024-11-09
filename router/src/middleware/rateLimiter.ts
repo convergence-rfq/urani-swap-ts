14372f85af0d4e818c059ba562821c3c85e44f12
Uwe Cerron <ucerron@selenis.co>
2024-11-09 11:10:34 -0500

    without router

--- router/src/middleware/rateLimiter.ts
+++
@@ -1,8 +0,0 @@
-import rateLimit from 'express-rate-limit';
-import { CONFIG } from '../config/config';
-
-export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: CONFIG.MAX_CONCURRENT_REQUESTS,
-  message: 'Too many requests from this IP, please try again later.',
-});
